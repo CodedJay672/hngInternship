6acf237efaea4e342c660c98c76d28b09a4d33af
codedjay672 <emmajoel6456@gmail.com>
2023-09-15 17:20:56 +0100

    output files

--- MovieBox/src/MovieCard/MovieCrew.js
+++ MovieBox/src/MovieCard/MovieCrew.js
@@ -1,11 +1,17 @@
 import React from "react";
 import './MovieCrew.css';
+import Photo from '../assets/Rectangle 37.png';
 
 export default function MovieCrew() {
   return (
-    <div className="btn-wrapper">
-      <button className="btn btn-primary">See Showtimes</button>
-      <button className="btn btn-secondary">More Watch Options</button>
-    </div>
+    <>
+      <div className="btn-wrapper">
+        <button className="btn btn-primary">See Showtimes</button>
+        <button className="btn btn-secondary">More Watch Options</button>
+        <div className="photo">
+          <img src={Photo} alt="crew photo" width="200" />
+        </div>
+      </div>
+    </>
   )
 }
